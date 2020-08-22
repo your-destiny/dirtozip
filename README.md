11c9f8e5f2545565f344f7073bc123458127fb7b
Artyom <artemmanvelyan1@gmail.com>
2020-08-22 18:38:43 +0900

    add subfolder in zip and update README.md

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # DIRTOZIP
 
-Python console application for zip directory archiving. You can pass 3 optional  parameters: 
+Python console application for zip directory archiving. You can pass 5 optional  parameters: 
 
-  1) Output file name. 
+  1) Output file name. <br> 
   If the output file name was not found, the name of the script will be used as the name
-  2) Input directory path
+  2) Input directory path <br>
   If the directory used to create the archive was not found, the directory with the script will be taken as the path
-  3) Output directory path
+  3) Output directory path <br>
   If the directory for the output file was not found, the directory with the script will be taken as the path
+  4) Skip files and subdirectories <br>
+  If you need not to include some subdirectories or files in the archive. Must be listed separated by commas
+  5) Skip output filename and script name <br>
+  Skipped by default, if you need not to be skipped, then type ['No', 'no', 'Not', 'not', 'N', 'n', '0', 'false', 'False',
+'FALSE',
+                                                       'f', 'F']
 
 
 ### Installation
@@ -17,12 +23,19 @@ You need to download the file and run it.
 ### Run
 Open cmd or powershell and type in the command line:
 ```sh
-python ./name_file.py NameArchive InputPath OutputPath
+python ./name_file.py NameArchive InputPath OutputPath SkipFiles SkipOutputFilenameAndScriptName
 ```
 Open bash and type in the command line:
 ```sh
-python3 ./name_file.py NameArchive InputPath OutputPath
+python3 ./name_file.py NameArchive InputPath OutputPath SkipFiles SkipOutputFilenameAndScriptName
 ```
+
+##### Example
+
+```sh
+python ./dirtozip.py App %cd% %cd% dirtozip.bat;.idea;.vscode n
+```
+
 Also you can create a .bat file and type in the command line for example:
 ```sh
 python ./name_file.py App %cd% %cd%
--- dirtozip.py
+++ dirtozip.py
@@ -1,15 +1,20 @@
-from zipfile import ZipFile
+import zipfile
 import os
 import sys
+
 
 def createZip():
     script_name = str(sys.argv[0]).split('/')[-1]
     zip_file_name = script_name.split('.')[0] + '.zip'
     path = str(os.path.dirname(os.path.abspath(__file__)))
     output_path = path
+    skip = None
+    skip_script_name_and_zip_file_name = True
     arg_warning_1 = "The output file name was not found, the name of the script will be used as the name"
-    arg_warning_2 = "The directory used to create the archive was not found, the directory with the script will be taken as the path"
-    arg_warning_3 = "The directory for the output file was not found, the directory with the script will be taken as the path"
+    arg_warning_2 = "The directory used to create the archive was not found, the directory with the script will be " \
+                    "taken as the path "
+    arg_warning_3 = "The directory for the output file was not found, the directory with the script will be taken as " \
+                    "the path "
 
     if len(sys.argv) == 2:
         zip_file_name = sys.argv[1] + '.zip'
@@ -23,21 +28,43 @@ def createZip():
         zip_file_name = sys.argv[1] + '.zip'
         path = str(sys.argv[2])
         output_path = str(sys.argv[3])
+        if len(sys.argv) >= 5:
+            if sys.argv[4] != '' and sys.argv[4].find(';') != -1:
+                skip = sys.argv[4].split(';')
+        if len(sys.argv) >= 6:
+            skip_script_name_and_zip_file_name_list = ['No', 'no', 'Not', 'not', 'N', 'n', '0', 'false', 'False',
+                                                       'FALSE',
+                                                       'f', 'F']
+            if sys.argv[5] in skip_script_name_and_zip_file_name_list:
+                skip_script_name_and_zip_file_name = False
     else:
         printWarning(arg_warning_1)
         printWarning(arg_warning_2)
         printWarning(arg_warning_3)
 
-    with ZipFile(output_path + '/' + zip_file_name, 'w') as zipObj:
-        for folderName, subfolders, filenames in os.walk(path):
+    if os.path.exists(path):
+        out_zipfile = zipfile.ZipFile(output_path + '/' + zip_file_name, 'w', zipfile.ZIP_DEFLATED)
+        for dir_path, dir_names, filenames in os.walk(path):
             for filename in filenames:
-                if filename == script_name or filename == script_name.split('.')[0] + '.bat' or filename == zip_file_name:
+                file_path = os.path.join(dir_path, filename)
+                parent_path = os.path.relpath(file_path, path)
+                if skip_script_name_and_zip_file_name and (parent_path == zip_file_name or parent_path == script_name):
                     continue
-                filePath = os.path.join(folderName, filename)
-                zipObj.write(filePath, os.path.basename(filePath))
+                check = False
+                if skip:
+                    for elem in skip:
+                        if parent_path.find(elem) != -1:
+                            check = True
+                            break
+                if check:
+                    continue
+                out_zipfile.write(file_path, parent_path)
+        out_zipfile.close()
+
 
 def printWarning(warning):
     print(warning)
+
 
 if __name__ == "__main__":
     createZip()
