bc60e0a86513608527dea699545011d67c47557c
Artyom <artemmanvelyan1@gmail.com>
2020-08-23 00:14:46 +0900

    add flags and different order

--- dirtozip.py
+++ dirtozip.py
@@ -4,43 +4,39 @@ import sys
 
 
 def createZip():
-    script_name = str(sys.argv[0]).split('/')[-1]
+    array_argv = sys.argv[1:]
+    script_name = str(array_argv[0]).split('/')[-1]
     zip_file_name = script_name.split('.')[0] + '.zip'
     path = str(os.path.dirname(os.path.abspath(__file__)))
     output_path = path
     skip = None
-    skip_script_name_and_zip_file_name = True
-    arg_warning_1 = "The output file name was not found, the name of the script will be used as the name"
-    arg_warning_2 = "The directory used to create the archive was not found, the directory with the script will be " \
+    skip_script_name_and_zip_file_name = False
+    ofn_warning_1 = "The output file name was not found, the name of the script will be used as the name"
+    idp_warning_2 = "The directory used to create the archive was not found, the directory with the script will be " \
                     "taken as the path "
-    arg_warning_3 = "The directory for the output file was not found, the directory with the script will be taken as " \
+    odp_warning_3 = "The directory for the output file was not found, the directory with the script will be taken as " \
                     "the path "
-
-    if len(sys.argv) == 2:
-        zip_file_name = sys.argv[1] + '.zip'
-        printWarning(arg_warning_2)
-        printWarning(arg_warning_3)
-    elif len(sys.argv) == 3:
-        zip_file_name = sys.argv[1] + '.zip'
-        path = str(sys.argv[2])
-        printWarning(arg_warning_3)
-    elif len(sys.argv) >= 4:
-        zip_file_name = sys.argv[1] + '.zip'
-        path = str(sys.argv[2])
-        output_path = str(sys.argv[3])
-        if len(sys.argv) >= 5:
-            if sys.argv[4] != '' and sys.argv[4].find(';') != -1:
-                skip = sys.argv[4].split(';')
-        if len(sys.argv) >= 6:
-            skip_script_name_and_zip_file_name_list = ['No', 'no', 'Not', 'not', 'N', 'n', '0', 'false', 'False',
-                                                       'FALSE',
-                                                       'f', 'F']
-            if sys.argv[5] in skip_script_name_and_zip_file_name_list:
-                skip_script_name_and_zip_file_name = False
+    if not checkIfExistArgv(array_argv, 'ofn'):
+        printWarning(ofn_warning_1)
     else:
-        printWarning(arg_warning_1)
-        printWarning(arg_warning_2)
-        printWarning(arg_warning_3)
+        zip_file_name = getArgvValue(array_argv, 'ofn') + '.zip'
+
+    if not checkIfExistArgv(array_argv, 'idp'):
+        printWarning(idp_warning_2)
+    else:
+        path = str(getArgvValue(array_argv, 'idp'))
+
+    if not checkIfExistArgv(array_argv, 'odp'):
+        printWarning(odp_warning_3)
+    else:
+        output_path = str(getArgvValue(array_argv, 'odp'))
+
+    if checkIfExistArgv(array_argv, 'sfs'):
+        if getArgvValue(array_argv, 'sfs') != '' and getArgvValue(array_argv, 'sfs').find(';') != -1:
+            skip = getArgvValue(array_argv, 'sfs').split(';')
+
+    if checkIfExistArgvWithoutValue(array_argv, 'sofs'):
+        skip_script_name_and_zip_file_name = True
 
     if os.path.exists(path):
         out_zipfile = zipfile.ZipFile(output_path + '/' + zip_file_name, 'w', zipfile.ZIP_DEFLATED)
@@ -64,6 +60,20 @@ def createZip():
 
 def printWarning(warning):
     print(warning)
+
+
+def getArgvValue(array_argv, elem):
+    return list(map(lambda x: x[1],
+                    [inner_arg for inner_arg in list(map(lambda x: x.split('--')[1].split('='), array_argv)) if
+                     len(inner_arg) > 1]))[list(map(lambda x: x.split('=')[0],[arg for arg in list(map(lambda x: x.split('--')[1], array_argv)) if arg.find('=') != -1])).index(elem)]
+
+
+def checkIfExistArgv(array_argv, elem):
+    return elem in list(map(lambda x: x.split('--')[1].split('=')[0], array_argv))
+
+
+def checkIfExistArgvWithoutValue(array_argv, elem):
+    return elem in [arg for arg in list(map(lambda x: x.split('--')[1], array_argv)) if arg.find('=') == -1]
 
 
 if __name__ == "__main__":
